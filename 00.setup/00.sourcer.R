95c71876ff66d2085beabaf246e6a69e3a279530
Alejandro Abraham <aleabraham6@gmail.com>
2021-12-11 17:18:41 +0100

    gitss

--- 00.setup/00.sourcer.R
+++ 00.setup/00.sourcer.R
@@ -8,22 +8,22 @@
 
 # Set up ===============================================================
 
-source(here::here('00.setup', "01.packages.R"))
-source(here::here('00.setup', "02.theme.R"))
-source(here::here('00.setup', "03.preparation.R"))
+source(file.path('00.setup', "01.packages.R"))
+source(file.path('00.setup', "02.theme.R"))
+source(file.path('00.setup', "03.preparation.R"))
 
 
 
 # UI ===============================================================
-source(here('01.ui', "main_panel.R"))
-source(here('01.ui', "A.sliders.R"))
-source(here('01.ui', "B.buttons.R"))
-source(here('01.ui', "sidebar_panel.R"))
-source(here('01.ui', "ui_app.R"))
+source(file.path('01.ui', "main_panel.R"))
+source(file.path('01.ui', "A.sliders.R"))
+source(file.path('01.ui', "B.buttons.R"))
+source(file.path('01.ui', "sidebar_panel.R"))
+source(file.path('01.ui', "ui_app.R"))
 
 
 # Source SERVER ===============================================================
-source(here('03.server', "server_app.R"))
+source(file.path('03.server', "server_app.R"))
 
 
    
